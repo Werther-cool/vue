cf068b7515862381b69fad02005ab0b3b60ea461
Werther <zhouwangyulu@icloud.com>
2017-05-17 10:32:13 +0800

    first commit

---
+++ mint-demo/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+<div id="app">
+  <mt-header fixed title="固定在顶部"></mt-header>
+  <mt-tabbar v-model="selected">
+    <mt-tab-item id="外卖">
+      <img slot="icon" src="./assets/logo.png"> 外卖
+    </mt-tab-item>
+    <mt-tab-item id="订单">
+      <img slot="icon" src="./assets/logo.png"> 订单
+    </mt-tab-item>
+    <mt-tab-item id="发现">
+      <img slot="icon" src="./assets/logo.png"> 发现
+    </mt-tab-item>
+    <mt-tab-item id="我的">
+      <img slot="icon" src="./assets/logo.png"> 我的
+    </mt-tab-item>
+  </mt-tabbar>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      selected: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
