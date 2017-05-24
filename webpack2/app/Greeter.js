ab06ae4298507d7e9876a7d1b381486c5252e106
Werther <zhouwangyulu@icloud.com>
2017-05-24 13:49:50 +0800

    webpack3

--- webpack2/app/Greeter.js
+++ webpack2/app/Greeter.js
@@ -1,5 +1,15 @@
-module.exports = function () {
-  var greet = document.creatElement('div');
-  greet.textContent = 'Hi there and greetings';
-  return greet;
+import React, {Component} from 'react';
+import config from './config.json';
+import styles from './Greeter.css';//导入
+
+class Greeter extends Component{
+  render() {
+    return (
+      <div className={styles.root}>//添加类名
+        {config.greetText}
+      </div>
+    );
+  }
 }
+
+export default Greeter
--- webpack2/webpack.config.js
+++ webpack2/webpack.config.js
@@ -1,14 +1,36 @@
 module.exports = {
-  devtool: 'eval-source-map',//配置生成Source Maps，选择合适的选项
+  devtool: 'eval-source-map',
+
   entry:  __dirname + "/app/main.js",
   output: {
     path: __dirname + "/public",
     filename: "bundle.js"
   },
+
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: "json-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',//在webpack的module部分的loaders里进行配置即可
+        query: {
+          presets: ['es2015','react']
+        }
+      },
+      {
+       test: /\.css$/,
+       loader: 'style-loader!css-loader?modules'//跟前面相比就在后面加上了?modules
+     }
+    ]
+  },
   devServer: {
-  contentBase: "./public",//本地服务器所加载的页面所在的目录
-  colors: true,//终端中输出结果为彩色
-  historyApiFallback: true,//不跳转
-  inline: true//实时刷新
-}
+    contentBase: "./public",
+    colors: true,
+    historyApiFallback: true,
+    inline: true
+  }
 }
