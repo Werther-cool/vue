898f8e5d1d87c1dd8e7eafd6a3a98f748e5ecf44
Werther <zhouwangyulu@icloud.com>
2017-05-26 11:51:31 +0800

    webpack

---
+++ vue-webpack-simple-demo2/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+<div id="app">
+  <h1>{{ msg }}</h1>
+  <button type="button" name="button" @click="change()">点击改变</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  method() {
+    change: {
+      // var str = this.msg;
+      // str = str.split('').reverse();
+      // this.msg = str;
+      console.log(1);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
