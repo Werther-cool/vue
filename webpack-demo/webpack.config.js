f260d5037b1b4eed544d9d6efea53e73d46c9e97
Werther <zhouwangyulu@icloud.com>
2017-05-26 09:09:18 +0800

    webpack

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,42 +1,21 @@
 // var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin'); //通过 npm 安装
 module.exports = {
-    entry:
-    {
-      main:'./src/script/main.js',
-      a:'./src/script/a.js',
-      b:'./src/script/b.js',
-      c:'./src/script/c.js',
-    },
+    entry:'./src/app.js',
     output:{
         path: __dirname + "/dist",
-        filename: 'js/[name]-[chunkhash].js'
+        filename: 'js/[name].bundle.js'
     },
+      module: {
+        loaders:[
+             {test: '/\.js$/', loader: 'babel-loader', options: {'presets': ['env']}}
+        ]
+      },
     plugins:[
      new HtmlWebpackPlugin({
-       filename:"a.html",
+       filename:"index.html",
        template: 'index.html',
-       inject:false,
-       title:'webpack is a',
-       chunks:['main','a'],
-       minify:{
-         removeComments:true,
-         collapseWhitespace:true
-       }
-     }),
-     new HtmlWebpackPlugin({
-       filename:"b.html",
-       template: 'index.html',
-       inject:false,
-       title:'webpack is b',
-       chunks:['main','b']
-     }),
-     new HtmlWebpackPlugin({
-       filename:"c.html",
-       template: 'index.html',
-       inject:false,
-       title:'webpack is c',
-       chunks:['main','c']
-     })
-    ]
+       inject:'body',
+     })]
+
 }
