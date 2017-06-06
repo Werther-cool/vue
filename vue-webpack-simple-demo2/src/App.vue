a8c56e9e1face40e030f89b5b123f8dd6a513459
Werther <zhouwangyulu@icloud.com>
2017-06-06 11:49:58 +0800

    vue

--- vue-webpack-simple-demo2/src/App.vue
+++ vue-webpack-simple-demo2/src/App.vue
@@ -1,6 +1,8 @@
 <template>
 <div id="app">
-  <h1>{{ msg }}</h1>
+  <p v-if="obj.array.length==0">123</p>
+  <h1 v-if="obj.array.length>0">{{ obj.array }}</h1>
+  <li v-for="item in obj.array">{{item}}</li>
   <button type="button" name="button" @click="change()">点击改变</button>
 </div>
 </template>
@@ -10,13 +12,25 @@ export default {
   name: 'app',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      obj: {
+        array: [1, 2, 3]
+      }
     }
   },
+  mounted: function() {
+    // var _this = this;
+    // this.$nextTick(function() {
+    //   _this.init();
+    // })
+  },
   method: {
-    change: function() {
-      console.log(1);
-    }
+    // init() {
+    //   console.log(obj.array.length);
+    // },
+    // change() {
+    //   console.log(1);
+    // },
+
   }
 }
 </script>
