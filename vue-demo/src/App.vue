e27168cd581b323972d2e367ba0869524db009c6
Werther <zhouwangyulu@icloud.com>
2017-06-08 17:41:32 +0800

    vue

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,31 +1,24 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<div id="app">
+  <!-- <div id="app">
+    <h1>Hello App!</h1>
+    <Nav></Nav>
+    <router-view></router-view>
+  </div> -->
+</div>
 </template>
 
 <script>
+import Nav from './components/nav.vue'
 export default {
   name: 'app',
-  data () {
+  data() {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  component: {
+    Nav
   }
 }
 </script>
@@ -40,7 +33,12 @@ export default {
   margin-top: 60px;
 }
 
-h1, h2 {
+.router-link-active {
+  color: red;
+}
+
+h1,
+h2 {
   font-weight: normal;
 }
 
---
+++ vue-demo/src/components/nav.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <p>
+    <router-link to="/foo">Go to Foo</router-link>
+    <router-link to="/bar">Go to Bar</router-link>
+  </p>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="css">
+</style>
