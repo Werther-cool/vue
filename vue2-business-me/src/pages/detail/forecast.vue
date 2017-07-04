08918855c111c6e7362987210f39917531e6d8aa
Werther <zhouwangyulu@icloud.com>
2017-07-04 09:14:41 +0800

    vue

---
+++ vue2-business-me/src/pages/detail/forecast.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="sales-board">
+  <h2>数据预测</h2>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- vue2-business-me/src/router/index.js
+++ vue2-business-me/src/router/index.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import IndexPage from '@/pages/index'
-
+import DetailPage from '@/pages/detail'
+import OrderListPage from '@/pages/orderList'
+import DetailAnaPage from '@/pages/detail/analysis'
+import DetailCouPage from '@/pages/detail/count'
+import DetailForPage from '@/pages/detail/forecast'
+import DetailPubPage from '@/pages/detail/publish'
 Vue.use(Router)
 
 export default new Router({
@@ -11,5 +16,32 @@ export default new Router({
 			path: '/',
 			component: IndexPage
 		},
+    {
+			path: '/orderList',
+			component: OrderListPage
+		},
+    {
+			path: '/detail',
+			component: DetailPage,
+			redirect: '/detail/analysis',
+			children: [
+				{
+					path: 'analysis',
+					component: DetailAnaPage
+				},
+				{
+					path: 'count',
+					component: DetailCouPage
+				},
+				{
+					path: 'forecast',
+					component: DetailForPage
+				},
+				{
+					path: 'publish',
+					component: DetailPubPage
+				}
+			]
+		}
   ]
 })
