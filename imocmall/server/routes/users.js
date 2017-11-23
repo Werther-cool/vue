3acbb807805e643b292682c5c4f0ae328439f84b
Werther <zhouwangyulu@icloud.com>
2017-11-23 11:38:05 +0800

    b

--- imocmall/server/routes/users.js
+++ imocmall/server/routes/users.js
@@ -8,4 +8,54 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/test',function (req,res,next) {
+  res.send('test');
+})
+
+router.post("login",function (req,res,next) {
+  var param = {
+    userName : req.body.userName,
+    userPwd : req.body.userPwd
+  }
+   User.findOne(param,function (err,doc) {
+     if (err) {
+       res.json({
+         status:"1",
+         msg:err.message
+       });
+     }else{
+       if (doc) {
+         res.cookie("userId",doc.userId,{
+           path:'/',
+           maxAge:1000*60*60
+         });
+         res.cookie("userId",doc.userName,{
+           path:'/',
+           maxAge:1000*60*60
+         });
+         res.json({
+           status:'0',
+           msg:'',
+           result:{
+             userName:doc.userName
+           }
+         })
+         
+       }
+     }
+     
+   }) 
+
+
+
+})
+
+
+
+
+
+
+
+
+
 module.exports = router;
